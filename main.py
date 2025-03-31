216c2a19530d53f45faa0527171cade12389d4cc
Fahed Oudeh <fahed.oudeh@gmail.com>
2025-03-31 10:13:40 +0200

    2 uit 3 van de bouns opdrach uit gewerkt. nog een te doen.

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-# maak een lijst met tuples
-studenten = [("Alice", 8.5), ("Bob", 7.0), ("Charlie", 6.2),
-("Diana", 9.1)]
-# print studenten en cifers
+studenten = []
+print("Welkom in studenten cijfers! voeg de naam en het cijfer")
+naam = input("voer eerst de naam in:\n ").capitalize()
+cijfer = float(input("voer het cijfer in:\n"))
+studenten.append((naam, cijfer))
+
+while True:
+    vraag = input("Wil je andere naam en cijfer invoeren? Ja/Nee\n").strip().lower()
+    if vraag != "ja":
+        break
+    else:
+        print("voeg de naam en het cijfer")
+        naam = input("voer eerst de naam in: ").capitalize()
+        cijfer = float(input("voer het cijfer in: "))
+        studenten.append((naam.capitalize(), cijfer))
+
+
+
+
+
+        # # maak een lijst met tuples
+        # studenten = [("Alice", 8.5), ("Bob", 7.0), ("Charlie", 6.2),
+        # ("Diana", 9.1)]
+        # # print studenten en cifers
 print("Studenten en hun cijfers: ")
+print(studenten)
+
 
 for naam, cijfer in studenten:
-    print(f"{naam} - {cijfer}")
+    print(f"{naam.capitalize()} - {cijfer}")
 
 # totaal score optellen
 
@@ -37,5 +59,22 @@ for naam, cijfer in studenten:
     if cijfer > beste_cijfer:
         beste_naam = naam
         beste_cijfer = cijfer
-        
-print(f"De beste student in {beste_naam} met een {beste_cijfer}")        
+
+print(f"De beste student is {beste_naam} met een {beste_cijfer}")      
+
+
+
+# Bonus 2: zoeken op naam van een student
+zoek_naam = input("Voer de naam van de student in die je wilt opzoeken: ").capitalize()
+
+gevonden = False
+
+for naam, cijfer in studenten:
+    if naam == zoek_naam:
+        print(f"{naam} heeft een {cijfer}")
+        gevonden = True
+        break
+
+if not gevonden:
+    print(f"Student {zoek_naam} is niet gevonden.")
+
